4ad5f7182272066ad99af0841dca4e214fda792a
evivar <42641415+evivar@users.noreply.github.com>
2019-02-13 17:59:42 +0100

    Modificado Galaga
    Falta ver como narices pintar las cosas donde me de la gana como los frames en clase

--- Galaga/engine.js
+++ Galaga/engine.js
@@ -1,304 +1,195 @@
+// Variables
+
+// Clases -> var NombreVar = new function() {}
 var SpriteSheet = new function () {
-    this.map = {};
-
-    this.load = function (spriteData, callback) {
-        this.map = spriteData;
-        this.image = new Image();
-        this.image.onload = callback;
-        this.image.src = 'Sprites/sprites.png';
-    };
-
-    this.draw = function (ctx, sprite, x, y, frame) {
-        var s = this.map[sprite];
-        if (!frame) frame = 0;
-        ctx.drawImage(this.image, s.sx + frame * s.w, s.sy, s.w, s.h, x, y, s.w, s.h);
-
-    };
-
-    this.render = function (ctx, sprite, x, y, frame) {
-        var s = this.map[sprite];
-        if (!frame) frame = 0;
-        ctx.clearRect(x, y, s.w, s.h);
-        ctx.fillStyle = "#FFFF00";
-        ctx.fillRect(x, y, s.w, s.h);
-        ctx.fillStyle = "rgba(0,0,128,0.5)";
-        ctx.fillRect(x, y, s.w, s.h);
-        ctx.drawImage(this.image, s.sx + frame * s.w, s.sy, s.w, s.h, x, y, s.w, s.h);
-    }
+  // Variables
+  this.map = {};
+  // Funciones
+  // Carga de los sprites
+  this.load = function (spriteData, callback) {
+    this.map = spriteData;
+    this.image = new Image();
+    this.image.onload = callback;
+    this.image.src = 'images/sprites.png';
+  };
+  // Dibuja un sprite especifico en una posicion
+  this.draw = function (ctx, sprite, x, y, frame) {
+    var s = this.map[sprite];
+    if (!frame) frame = 0;
+    ctx.drawImage(this.image,
+      s.sx + frame * s.w,
+      s.sy,
+      s.w, s.h,
+      x, y,
+      s.w, s.h);
+  }
+  // Dibuja la animacion de un sprite
+  this.render = function (ctx, sprite, x, y, frame) {
+    var s = this.map[sprite];
+    if (!frame) frame = 0;
+    ctx.clearRect(x, y, s.w, s.h);
+    ctx.fillStyle = "#FFFF00";
+    ctx.fillRect(x, y, s.w, s.h);
+    ctx.fillStyle = "rgba(0,0,128,0.5)";
+    ctx.fillRect(x, y, s.w, s.h);
+    ctx.drawImage(this.image, s.sx + frame * s.w, s.sy, s.w, s.h, x, y, s.w, s.h);
+  };
 }
 
-(function() {
-    var lastTime = 0;
-    var vendors = ['ms', 'moz', 'webkit', 'o'];
-    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-        window.cancelAnimationFrame = 
-          window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
-    }
- 
-    if (!window.requestAnimationFrame)
-        window.requestAnimationFrame = function(callback, element) {
-            var currTime = new Date().getTime();
-            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
-              timeToCall);
-            lastTime = currTime + timeToCall;
-            return id;
-        };
- 
-    if (!window.cancelAnimationFrame)
-        window.cancelAnimationFrame = function(id) {
-            clearTimeout(id);
-        };
-}());
-  
-
-var Game = new function() {                                                                  
+var Game = new function () {
+  // Variables
   var boards = [];
+  // le asignamos un nombre lógico a cada tecla que nos interesa
+  var KEY_CODES = { 37: 'left', 39: 'right', 32: 'fire' };
 
-  // Game Initialization
-  this.initialize = function(canvasElementId,sprite_data,callback) {
-    this.canvas = document.getElementById(canvasElementId);
-
-    this.playerOffset = 10;
-    this.canvasMultiplier= 1;
-    this.setupMobile();
-
-    this.width = this.canvas.width;
-    this.height= this.canvas.height;
-
-    this.ctx = this.canvas.getContext && this.canvas.getContext('2d');
-    if(!this.ctx) { return alert("Please upgrade your browser to play"); }
-
-    this.setupInput();
-
-    this.loop(); 
-
-    if(this.mobile) {
-      this.setBoard(4,new TouchControls());
-    }
-
-    SpriteSheet.load(sprite_data,callback);
-  };
-  
-
-  // Handle Input
-  var KEY_CODES = { 37:'left', 39:'right', 32 :'fire' };
   this.keys = {};
+  //Funciones
 
-  this.setupInput = function() {
-    window.addEventListener('keydown',function(e) {
-      if(KEY_CODES[e.keyCode]) {
-       Game.keys[KEY_CODES[e.keyCode]] = true;
-       e.preventDefault();
-      }
-    },false);
-
-    window.addEventListener('keyup',function(e) {
-      if(KEY_CODES[e.keyCode]) {
-       Game.keys[KEY_CODES[e.keyCode]] = false; 
-       e.preventDefault();
-      }
-    },false);
+  // Inicialización del juego
+  // se obtiene el canvas, se cargan los recursos y se llama a callback
+  this.initialize = function (canvasElementId, sprite_data, callback) {
+    this.canvas = document.getElementById(canvasElementId)
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
+    this.ctx = this.canvas.getContext && this.canvas.getContext('2d');
+    if (!this.ctx) {
+      return alert("Please upgrade your browser to play");
+    }
+    this.setupInput();
+    this.loop();
+    SpriteSheet.load(sprite_data, callback);
   };
 
+  this.setupInput = function () {
+    window.addEventListener('keydown', function (e) {
+      if (KEY_CODES[e.keyCode]) {
+        Game.keys[KEY_CODES[e.keyCode]] = true;
+        e.preventDefault();
+      }
+    }, false);
+    window.addEventListener('keyup', function (e) {
+      if (KEY_CODES[e.keyCode]) {
+        Game.keys[KEY_CODES[e.keyCode]] = false;
+        e.preventDefault();
+      }
+    }, false);
+  }
 
-  var lastTime = new Date().getTime();
-  var maxTime = 1/30;
-  // Game Loop
-  this.loop = function() { 
-    var curTime = new Date().getTime();
-    requestAnimationFrame(Game.loop);
-    var dt = (curTime - lastTime)/1000;
-    if(dt > maxTime) { dt = maxTime; }
-
-    for(var i=0,len = boards.length;i<len;i++) {
-      if(boards[i]) { 
+  this.loop = function () {
+    var dt = 30 / 1000;
+    // Cada pasada borramos el canvas
+    Game.ctx.fillStyle = "#000";
+    Game.ctx.fillRect(0, 0, Game.width, Game.height);
+    // y actualizamos y dibujamos todas las entidades
+    for (var i = 0, len = boards.length; i < len; i++) {
+      if (boards[i]) {
         boards[i].step(dt);
         boards[i].draw(Game.ctx);
       }
     }
-    lastTime = curTime;
+    setTimeout(Game.loop, 30);
   };
-  
+
+
   // Change an active game board
-  this.setBoard = function(num,board) { boards[num] = board; };
-
-
-  this.setupMobile = function() {
-    var container = document.getElementById("container"),
-        hasTouch =  !!('ontouchstart' in window),
-        w = window.innerWidth, h = window.innerHeight;
-      
-    if(hasTouch) { this.mobile = true; }
-
-    if(screen.width >= 1280 || !hasTouch) { return false; }
-
-    if(w > h) {
-      alert("Please rotate the device and then click OK");
-      w = window.innerWidth; h = window.innerHeight;
-    }
-
-    container.style.height = h*2 + "px";
-    window.scrollTo(0,1);
-
-    h = window.innerHeight + 2;
-    container.style.height = h + "px";
-    container.style.width = w + "px";
-    container.style.padding = 0;
-
-    if(h >= this.canvas.height * 1.75 || w >= this.canvas.height * 1.75) {
-      this.canvasMultiplier = 2;
-      this.canvas.width = w / 2;
-      this.canvas.height = h / 2;
-      this.canvas.style.width = w + "px";
-      this.canvas.style.height = h + "px";
-    } else {
-      this.canvas.width = w;
-      this.canvas.height = h;
-    }
-
-    this.canvas.style.position='absolute';
-    this.canvas.style.left="0px";
-    this.canvas.style.top="0px";
-
+  this.setBoard = function (num, board) {
+    boards[num] = board;
   };
-
 };
 
+// Funciones
 
-var SpriteSheet = new function() {
-  this.map = { }; 
-
-  this.load = function(spriteData,callback) { 
-    this.map = spriteData;
-    this.image = new Image();
-    this.image.onload = callback;
-    this.image.src = 'Sprites/sprites.png';
-  };
-
-  this.draw = function(ctx,sprite,x,y,frame) {
-    var s = this.map[sprite];
-    if(!frame) frame = 0;
-    ctx.drawImage(this.image,
-                     s.sx + frame * s.w, 
-                     s.sy, 
-                     s.w, s.h, 
-                     Math.floor(x), Math.floor(y),
-                     s.w, s.h);
-  };
-
-  return this;
-};
-
-var TitleScreen = function TitleScreen(title,subtitle,callback) {
+var TitleScreen = function TitleScreen(title, subtitle, callback) {
   var up = false;
-  this.step = function(dt) {
-    if(!Game.keys['fire']) up = true;
-    if(up && Game.keys['fire'] && callback) callback();
+
+  this.step = function (dt) {
+    if (!Game.keys['fire']) up = true;
+    if (up && Game.keys['fire'] && callback) callback();
   };
 
-  this.draw = function(ctx) {
+  this.draw = function (ctx) {
     ctx.fillStyle = "#FFFFFF";
-
+    ctx.textAlign = "center";
     ctx.font = "bold 40px bangers";
-    var measure = ctx.measureText(title);  
-    ctx.fillText(title,Game.width/2 - measure.width/2,Game.height/2);
-
+    ctx.fillText(title, Game.width / 2, Game.height / 2);
     ctx.font = "bold 20px bangers";
-    var measure2 = ctx.measureText(subtitle);
-    ctx.fillText(subtitle,Game.width/2 - measure2.width/2,Game.height/2 + 40);
+    ctx.fillText(subtitle, Game.width / 2, Game.height / 2 + 140);
   };
 };
 
-
-var GameBoard = function() {
+var GameBoard = function () {
   var board = this;
-
   // The current list of objects
   this.objects = [];
   this.cnt = {};
-
   // Add a new object to the object list
-  this.add = function(obj) { 
-    obj.board=this; 
-    this.objects.push(obj); 
+  this.add = function (obj) {
+    obj.board = this;
+    this.objects.push(obj);
     this.cnt[obj.type] = (this.cnt[obj.type] || 0) + 1;
-    return obj; 
+    return obj;
   };
-
+  // Reset the list of removed objects
+  this.resetRemoved = function () { this.removed = []; };
   // Mark an object for removal
-  this.remove = function(obj) { 
+  this.remove = function (obj) {
     var idx = this.removed.indexOf(obj);
-    if(idx == -1) {
-      this.removed.push(obj); 
+    if (idx == -1) {
+      this.removed.push(obj);
       return true;
     } else {
       return false;
     }
   };
-
-  // Reset the list of removed objects
-  this.resetRemoved = function() { this.removed = []; };
-
   // Removed an objects marked for removal from the list
-  this.finalizeRemoved = function() {
-    for(var i=0,len=this.removed.length;i<len;i++) {
+  this.finalizeRemoved = function () {
+    for (var i = 0, len = this.removed.length; i < len; i++) {
       var idx = this.objects.indexOf(this.removed[i]);
-      if(idx != -1) {
+      if (idx != -1) {
         this.cnt[this.removed[i].type]--;
-        this.objects.splice(idx,1);
+        this.objects.splice(idx, 1);
       }
     }
   };
-
-  // Call the same method on all current objects 
-  this.iterate = function(funcName) {
-     var args = Array.prototype.slice.call(arguments,1);
-     for(var i=0,len=this.objects.length;i<len;i++) {
-       var obj = this.objects[i];
-       obj[funcName].apply(obj,args);
-     }
+  // Call the same method on all current objects
+  this.iterate = function (funcName) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    for (var i = 0, len = this.objects.length; i < len; i++) {
+      var obj = this.objects[i];
+      obj[funcName].apply(obj, args);
+    }
   };
-
   // Find the first object for which func is true
-  this.detect = function(func) {
-    for(var i = 0,val=null, len=this.objects.length; i < len; i++) {
-      if(func.call(this.objects[i])) return this.objects[i];
+  this.detect = function (func) {
+    for (var i = 0, val = null, len = this.objects.length; i < len; i++) {
+      if (func.call(this.objects[i])) return this.objects[i];
     }
     return false;
   };
-
   // Call step on all objects and them delete
   // any object that have been marked for removal
-  this.step = function(dt) { 
+  this.step = function (dt) {
     this.resetRemoved();
-    this.iterate('step',dt);
+    this.iterate('step', dt);
     this.finalizeRemoved();
   };
-
   // Draw all the objects
-  this.draw= function(ctx) {
-    this.iterate('draw',ctx);
+  this.draw = function (ctx) {
+    this.iterate('draw', ctx);
   };
+};
 
-  // Check for a collision between the 
-  // bounding rects of two objects
-  this.overlap = function(o1,o2) {
-    return !((o1.y+o1.h-1<o2.y) || (o1.y>o2.y+o2.h-1) ||
-             (o1.x+o1.w-1<o2.x) || (o1.x>o2.x+o2.w-1));
-  };
+var FrameRate = function () {
 
-  // Find the first object that collides with obj
-  // match against an optional type
-  this.collide = function(obj,type) {
-    return this.detect(function() {
-      if(obj != this) {
-       var col = (!type || this.type & type) && board.overlap(obj,this);
-       return col ? this : false;
-      }
-    });
-  };
+  this.draw = function (ctx) {
+    ctx.save();
+    ctx.font = "bold 18px arial";
+    ctx.fillStyle = "#FFFFFF";
+
+    var txt = "Holi";
+
+    ctx.fillText(txt, 10, 20);
+  }
 
 
 };
--- Galaga/game.js
+++ Galaga/game.js
@@ -1,35 +1,13 @@
+// Variables
 var sprites = {
     ship: { sx: 0, sy: 0, w: 37, h: 42, frames: 1 },
-    missile: { sx: 0, sy: 30, w: 2, h: 10, frames: 1 },
-    enemy_purple: { sx: 37, sy: 0, w: 42, h: 43, frames: 1 },
-    enemy_bee: { sx: 79, sy: 0, w: 37, h: 43, frames: 1 },
-    enemy_ship: { sx: 116, sy: 0, w: 42, h: 43, frames: 1 },
-    enemy_circle: { sx: 158, sy: 0, w: 32, h: 33, frames: 1 },
-    explosion: { sx: 0, sy: 64, w: 64, h: 64, frames: 12 },
-    enemy_missile: { sx: 9, sy: 42, w: 3, h: 20, frame: 1, }
-};
-
-var enemies = {
-    straight: {
-        x: 0, y: -50, sprite: 'enemy_ship', health: 10,
-        E: 100
-    },
-    ltr: {
-        x: 0, y: -100, sprite: 'enemy_purple', health: 10,
-        B: 75, C: 1, E: 100, missiles: 2
-    },
-    circle: {
-        x: 250, y: -50, sprite: 'enemy_circle', health: 10,
-        A: 0, B: -100, C: 1, E: 20, F: 100, G: 1, H: Math.PI / 2
-    },
-    wiggle: {
-        x: 100, y: -50, sprite: 'enemy_bee', health: 20,
-        B: 50, C: 4, E: 100, firePercentage: 0.001, missiles: 2
-    },
-    step: {
-        x: 0, y: -50, sprite: 'enemy_circle', health: 10,
-        B: 150, C: 1.2, E: 75
-    }
+    enemy_ship_purple: { sx: 37, sy: 0, w: 42, h: 43, frames: 1 },
+    enemy_ship_orange: { sx: 79, sy: 0, w: 37, h: 43, frames: 1 },
+    enemy_ship_gray: { sx: 116, sy: 0, w: 42, h: 43, frames: 1 },
+    enemy_green_ball: { sx: 158, sy: 0, w: 32, h: 33, frames: 1 },
+    player_missile: { sx: 0, sy: 42, w: 7, h: 20, frames: 1 },
+    enemy_missile: { sx: 9, sy: 42, w: 3, h: 20, frames: 1 },
+    explosion: { sx: 0, sy: 64, w: 64, h: 64, frames: 12 }
 };
 
 var OBJECT_PLAYER = 1,
@@ -38,157 +16,92 @@ var OBJECT_PLAYER = 1,
     OBJECT_ENEMY_PROJECTILE = 8,
     OBJECT_POWERUP = 16;
 
-var startGame = function () {
-    var ua = navigator.userAgent.toLowerCase();
 
-    // Only 1 row of stars
-    if (ua.match(/android/)) {
-        Game.setBoard(0, new Starfield(50, 0.6, 100, true));
-    } else {
-        Game.setBoard(0, new Starfield(20, 0.4, 100, true));
-        Game.setBoard(1, new Starfield(50, 0.6, 100));
-        Game.setBoard(2, new Starfield(100, 1.0, 50));
-    }
-    Game.setBoard(3, new TitleScreen("Alien Invasion",
+// Funciones
+
+// Especifica lo que se debe pintar al cargar el juego
+var startGame = function () {
+    Game.setBoard(0, new TitleScreen("Alien Invasion",
         "Press fire to start playing",
         playGame));
-};
-
-var level1 = [
-    // Start,   End, Gap,  Type,   Override
-    [0, 4000, 500, 'step'],
-    [6000, 13000, 800, 'ltr'],
-    [10000, 16000, 400, 'circle'],
-    [17800, 20000, 500, 'straight', { x: 50 }],
-    [18200, 20000, 500, 'straight', { x: 90 }],
-    [18200, 20000, 500, 'straight', { x: 10 }],
-    [22000, 25000, 400, 'wiggle', { x: 150 }],
-    [22000, 25000, 400, 'wiggle', { x: 100 }]
-];
-
-
-
+}
 var playGame = function () {
     var board = new GameBoard();
     board.add(new PlayerShip());
-    board.add(new Level(level1, winGame));
-    Game.setBoard(3, board);
-    Game.setBoard(5, new GamePoints(0));
+    Game.setBoard(0, board);
 };
 
-var winGame = function () {
-    Game.setBoard(3, new TitleScreen("You win!",
-        "Press fire to play again",
-        playGame));
-};
-
-var loseGame = function () {
-    Game.setBoard(3, new TitleScreen("You lose!",
-        "Press fire to play again",
-        playGame));
-};
-
-var Starfield = function (speed, opacity, numStars, clear) {
-
-    // Set up the offscreen canvas
-    var stars = document.createElement("canvas");
-    stars.width = Game.width;
-    stars.height = Game.height;
-    var starCtx = stars.getContext("2d");
-
-    var offset = 0;
-
-    // If the clear option is set, 
-    // make the background black instead of transparent
-    if (clear) {
-        starCtx.fillStyle = "#000";
-        starCtx.fillRect(0, 0, stars.width, stars.height);
-    }
-
-    // Now draw a bunch of random 2 pixel
-    // rectangles onto the offscreen canvas
-    starCtx.fillStyle = "#FFF";
-    starCtx.globalAlpha = opacity;
-    for (var i = 0; i < numStars; i++) {
-        starCtx.fillRect(Math.floor(Math.random() * stars.width),
-            Math.floor(Math.random() * stars.height),
-            2,
-            2);
-    }
-
-    // This method is called every frame
-    // to draw the starfield onto the canvas
-    this.draw = function (ctx) {
-        var intOffset = Math.floor(offset);
-        var remaining = stars.height - intOffset;
-
-        // Draw the top half of the starfield
-        if (intOffset > 0) {
-            ctx.drawImage(stars,
-                0, remaining,
-                stars.width, intOffset,
-                0, 0,
-                stars.width, intOffset);
-        }
-
-        // Draw the bottom half of the starfield
-        if (remaining > 0) {
-            ctx.drawImage(stars,
-                0, 0,
-                stars.width, remaining,
-                0, intOffset,
-                stars.width, remaining);
-        }
-    };
-
-    // This method is called to update
-    // the starfield
-    this.step = function (dt) {
-        offset += dt * speed;
-        offset = offset % stars.height;
-    };
-};
+// Indica que se llame al método de inicialización una vez
+// se haya terminado de cargar la página HTML
+// y este después de realizar la inicialización llamará a
+// startGame
+window.addEventListener("load", function () {
+    Game.initialize("game", sprites, startGame);
+});
 
 var PlayerShip = function () {
-    this.setup('ship', { vx: 0, reloadTime: 0.25, maxVel: 200 });
-
-    this.reload = this.reloadTime;
+    this.w = SpriteSheet.map['ship'].w;
+    this.h = SpriteSheet.map['ship'].h;
     this.x = Game.width / 2 - this.w / 2;
-    this.y = Game.height - Game.playerOffset - this.h;
-
+    this.y = Game.height - 10 - this.h;
+    this.vx = 0;
+    this.maxVel = 200;
     this.step = function (dt) {
         if (Game.keys['left']) { this.vx = -this.maxVel; }
         else if (Game.keys['right']) { this.vx = this.maxVel; }
         else { this.vx = 0; }
-
         this.x += this.vx * dt;
-
         if (this.x < 0) { this.x = 0; }
         else if (this.x > Game.width - this.w) {
-            this.x = Game.width - this.w;
+            this.x = Game.width - this.w
         }
-
-        this.reload -= dt;
-        if (Game.keys['fire'] && this.reload < 0) {
-            Game.keys['fire'] = false;
-            this.reload = this.reloadTime;
-
-            this.board.add(new PlayerMissile(this.x, this.y + this.h / 2));
-            this.board.add(new PlayerMissile(this.x + this.w, this.y + this.h / 2));
-        }
-    };
-};
-
-PlayerShip.prototype = new Sprite();
-PlayerShip.prototype.type = OBJECT_PLAYER;
-
-PlayerShip.prototype.hit = function (damage) {
-    if (this.board.remove(this)) {
-        loseGame();
     }
-};
+    this.draw = function (ctx) {
+        SpriteSheet.draw(ctx, 'ship', this.x, this.y, 0);
+    }
+}
 
-window.addEventListener("load", function () {
-    Game.initialize("game", sprites, startGame);
-});
+/*function startGame() {
+    // Pinto el fondo que son 3 rectangulos de distintos colores
+    ctx.fillStyle = "#FFFF00";
+    ctx.fillRect(50, 100, 380, 400);
+    ctx.fillStyle = "rgba(0,0,128,0.5)";
+    ctx.fillRect(25, 50, 380, 400);
+    // Pinto la nave en 3 posiciones distintas
+    SpriteSheet.load({
+        ship: { sx: 0, sy: 0, w: 38, h: 43, frames: 3 },
+        explosion: { sx: 0, sy: 64, w: 64, h: 64, frames: 12}
+    }, function () {
+        SpriteSheet.draw(ctx, "ship", 0, 0);
+    });
+
+}*/
+
+function drawExplosion_Low2High() {
+    var fire = 13;
+    var id = setInterval(explosion, 50);
+    function explosion() {
+        if (fire == -1) {
+            clearInterval(id);
+        }
+        else {
+            fire--;
+            SpriteSheet.render(ctx, "explosion", 150, 100, fire);
+        }
+    }
+}
+
+function drawExplosion_High2Low() {
+    var fire = 0;
+    var id = setInterval(explosion, 50);
+    function explosion() {
+        if (fire > 12) {
+            clearInterval(id);
+        }
+        else {
+            fire++;
+            SpriteSheet.render(ctx, "explosion", 250, 100, fire);
+        }
+    }
+}
 
